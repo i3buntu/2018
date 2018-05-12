cc338694630caaa2cc83d50e0bb3be543ca9a950
i3buntu <i3wmbuntu@gmail.com>
2018-05-11 21:43:02 -0600

    Add files via upload

---
+++ dotfiles/config/i3/repara.sh
@@ -0,0 +1,80 @@
+#!/bin/bash 
+#
+# Developed by Roberto Flores
+# Contact mr.pilot.att@gmail.com
+#
+# set -xv
+
+repara.sh(){
+#
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc5
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc6
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc7
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc8
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc9
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc10
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc11
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc12
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc13
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc14
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc15
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc16
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc17 
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc18
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc19 
+#sudo e2fsck -fpcv -E fragcheck /dev/sdc20
+
+#sudo e2fsck -pcv /dev/sdc5
+#sudo e2fsck -pcv /dev/sdc6
+#sudo e2fsck -pcv /dev/sdc7
+#sudo e2fsck -pcv /dev/sdc8
+#sudo e2fsck -pcv /dev/sdc9
+#sudo e2fsck -pcv /dev/sdc10
+#sudo e2fsck -pcv /dev/sdc11
+#sudo e2fsck -pcv /dev/sdc12
+#sudo e2fsck -pcv /dev/sdc13
+#sudo e2fsck -pcv /dev/sdc14
+#sudo e2fsck -pcv /dev/sdc15
+#sudo e2fsck -pcv /dev/sdc16
+#sudo e2fsck -pcv /dev/sdc17 
+#sudo e2fsck -pcv /dev/sdc18
+#sudo e2fsck -pcv /dev/sdc19 
+#sudo e2fsck -pcv /dev/sdc20
+
+
+#sudo e2fsck -fcyv /dev/sdc5
+#sudo e2fsck -fcyv /dev/sdc6
+#sudo e2fsck -fcyv /dev/sdc7
+#sudo e2fsck -fcyv /dev/sdc8
+#sudo e2fsck -fcyv /dev/sdc9
+#sudo e2fsck -fcyv /dev/sdc10
+#sudo e2fsck -fcyv /dev/sdc11
+#sudo e2fsck -fcyv /dev/sdc12
+#sudo e2fsck -fcyv /dev/sdc13
+#sudo e2fsck -fcyv /dev/sdc14
+#sudo e2fsck -fcyv /dev/sdc15
+#sudo e2fsck -fcyv /dev/sdc16
+#sudo e2fsck -fcyv /dev/sdc17 
+#sudo e2fsck -fcyv /dev/sdc18
+#sudo e2fsck -fcyv /dev/sdc19 
+#sudo e2fsck -fcyv /dev/sdc20
+
+sudo e2fsck -fy /dev/sdc5;
+sudo e2fsck -fy /dev/sdc6;
+sudo e2fsck -fy /dev/sdc7;
+sudo e2fsck -fy /dev/sdc8;
+sudo e2fsck -fy /dev/sdc9;
+sudo e2fsck -fy /dev/sdc10;
+sudo e2fsck -fy /dev/sdc11;
+sudo e2fsck -fy /dev/sdc12;
+sudo e2fsck -fy /dev/sdc13;
+sudo e2fsck -fy /dev/sdc14;
+sudo e2fsck -fy /dev/sdc15;
+sudo e2fsck -fy /dev/sdc16;
+sudo e2fsck -fy /dev/sdc17;
+sudo e2fsck -fy /dev/sdc18;
+sudo e2fsck -fy /dev/sdc19; 
+sudo e2fsck -fy /dev/sdc20;
+#
+#
+}
