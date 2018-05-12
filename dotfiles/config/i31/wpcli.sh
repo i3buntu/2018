f39bbae7c2d708e9f0228fd180f3a765a25bf745
i3buntu <i3wmbuntu@gmail.com>
2018-05-11 21:41:28 -0600

    Add files via upload

---
+++ dotfiles/config/i31/wpcli.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Developed by Rafael Corrêa Gomes
+# Contact rafaelcgstz@gmail.com
+#
+
+wpcli.sh(){
+	
+	sudo e2fsck -fy /dev/sdc5;
+	sudo e2fsck -fy /dev/sdc6 
+	sudo e2fsck -fy /dev/sdc7;
+	sudo e2fsck -fy /dev/sdc8;
+	sudo e2fsck -fy /dev/sdc9;
+	sudo e2fsck -fy /dev/sdc10;
+	sudo e2fsck -fy /dev/sdc11;
+	sudo e2fsck -fy /dev/sdc12;
+	sudo e2fsck -fy /dev/sdc13;
+	sudo e2fsck -fy /dev/sdc14;
+	sudo e2fsck -fy /dev/sdc15;
+	sudo e2fsck -fy /dev/sdc16;
+	sudo e2fsck -fy /dev/sdc17;
+	sudo e2fsck -fy /dev/sdc18;
+	sudo e2fsck -fy /dev/sdc19; 
+	sudo e2fsck -fy /dev/sdc20;
+
+# sudo e2fsck -fy /dev/sdc6 && sudo e2fsck -fy /dev/sdc7 && sudo e2fsck -fy /dev/sdc8 && sudo e2fsck -fy /dev/sdc9 && sudo e2fsck -fy /dev/sdc10 && sudo e2fsck -fy /dev/sdc11 && sudo e2fsck -fy /dev/sdc12 && sudo e2fsck -fy /dev/sdc13 && sudo e2fsck -fy /dev/sdc14 && sudo e2fsck -fy /dev/sdc15 && sudo e2fsck -fy /dev/sdc16 && sudo e2fsck -fy /dev/sdc17 && sudo e2fsck -fy /dev/sdc18 && sudo e2fsck -fy /dev/sdc19 && sudo e2fsck -fy /dev/sdc20	
+}
